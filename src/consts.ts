803eb2d6425fedbd0f37fb03f4e26ccbd57b30c5
Daniell Wijdenbosch <d.wijdenbosch@icloud.com>
2024-02-05 21:28:23 +0100

    feat: add linkedin link

--- src/consts.ts
+++ src/consts.ts
@@ -5,5 +5,6 @@ export const SITE_URL = "https://daniell.dev";
 export const GITHUB_USERNAME = "daniellwdb";
 export const CLOUDINARY_CLOUD_NAME = "daniellwdb";
 export const MIDDLE_DOT = "\u00B7";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/daniellwijdenbosch/";
 
 export const HEADING_LINK_ANCHOR = `before:content-['#'] before:absolute before:-ml-[1em] before:text-dark-gray/0 hover:before:text-dark-gray/50 pl-[1em] -ml-[1em] no-underline`;
