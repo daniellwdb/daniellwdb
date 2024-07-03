009cb19c886494f8c6c28b1c23872c32a4b595de
Daniell Wijdenbosch <d.wijdenbosch@icloud.com>
2024-07-03 20:53:10 +0200

    feat: update spotify endpoint header

--- src/pages/api/spotify.ts
+++ src/pages/api/spotify.ts
@@ -85,5 +85,10 @@ export const GET: APIRoute = async () => {
         currentlyPlayingTrack && formatTrack(currentlyPlayingTrack),
       topTracks: userTop.items.map(formatTrack),
     } satisfies APIResponse),
+    {
+      headers: {
+        "Cache-Control": "public, s-maxage=60, stale-while-revalidate=30",
+      },
+    },
   );
 };
