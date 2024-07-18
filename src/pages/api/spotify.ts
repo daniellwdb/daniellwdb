951d4d46cc72c85c5d3dcef11a8797df13c1a270
Daniell Wijdenbosch <d.wijdenbosch@icloud.com>
2024-07-18 12:10:58 +0200

    feat: change cache control header

--- src/pages/api/spotify.ts
+++ src/pages/api/spotify.ts
@@ -89,7 +89,7 @@ export const GET: APIRoute = async () => {
     } satisfies APIResponse),
     {
       headers: {
-        "Cache-Control": "public, s-maxage=60, stale-while-revalidate=30",
+        "Cache-Control": "public, maxage=60, stale-while-revalidate=30",
       },
     },
   );
